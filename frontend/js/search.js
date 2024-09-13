148577175186b35919e9d350d351c7328bfe6d29
Fatima-ezzahra ELKAWKABI <fatizahra.elkawkabi@gmail.com>
2024-09-13 16:06:15 +0100

    ajustingslider desing

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,5 +1,8 @@
 const search_res_button = document.getElementById('responsive-search-btn')
-const search_page = document.getELementById('search-page') ;
+const close_search_btn = document.getElementById('close-search')
+const search_page = document.getElementById('search-page') ;
 function toggleRespSearch(){
-    
+    search_page.classList.toggle('visible')
 }
+search_res_button.addEventListener('click',toggleRespSearch)
+close_search_btn.addEventListener('click',toggleRespSearch)
