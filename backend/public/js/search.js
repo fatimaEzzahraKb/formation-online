0ab2faa913d9158dcedf29e1ff1842bbfb29401f
Fatima-ezzahra ELKAWKABI <fatizahra.elkawkabi@gmail.com>
2024-10-05 11:18:12 +0100

    suppression des dossiers

--- backend/public/js/search.js
+++
@@ -1,8 +0,0 @@
-const search_res_button = document.getElementById('responsive-search-btn')
-const close_search_btn = document.getElementById('close-search')
-const search_page = document.getElementById('search-page') ;
-function toggleRespSearch(){
-    search_page.classList.toggle('visible')
-}
-search_res_button.addEventListener('click',toggleRespSearch)
-close_search_btn.addEventListener('click',toggleRespSearch)
